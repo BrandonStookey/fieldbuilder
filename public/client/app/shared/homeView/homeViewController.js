d059c990bfe16a1be5b73a83a9e0ad71210032a8
Brandon Stookey <bwstookey@gmail.com>
2016-07-13 00:15:40 -0400

    Cleaning up codes and comments

--- public/client/app/shared/homeView/homeViewController.js
+++ public/client/app/shared/homeView/homeViewController.js
@@ -5,88 +5,89 @@ angular.module('project.homeView', ['ui.bootstrap', 'ngAnimate', 'ngSanitize','a
 .controller('homeViewController', ['$scope', 'projectFactory', function($scope, projectFactory){
 	console.log('I am homeViewController!!!');
 	
-	$scope.color; 
 	$scope.maxlength = 5;
-	$scope.fieldServiceResult;
 	$scope.label;
 	$scope.default;
-	$scope.choices;
 	$scope.choice = '';
-	$scope.count = 0;
-   
-	console.log($scope.fieldServiceResult);
-
+//===================Makes an API request to auto-fill form
 	projectFactory.getFieldService().then(function(result){
-		$scope.choices = result.data.choices;
-		$scope.fieldServiceResult = result.data
-		$scope.label = $scope.fieldServiceResult.label;
-		$scope.default = $scope.fieldServiceResult.default;
+		$scope.label = result.data.label;
+		$scope.default = result.data.default;
 
-		for(var i = 0; i < $scope.choices.length; i++){
-			$scope.choice += $scope.choices[i] + '\n';
+		for(var i = 0; i < result.data.choices.length; i++){
+			$scope.choice += result.data.choices[i] + '\n';
 		}
-
-		console.log('result: ', result.data.label);
-	
 	});
-
+//====================Allows user to clear input field by clicking on 'cancel'
   $scope.clearFields = function(){
     $scope.label = '';
     $scope.default = '';
     $scope.choice = '';
   }
-
+//====================Collects form info, then invokes $http request in app.module.js
 	$scope.formInfo = function(label, default1, choices, order){
-		if(order === 'options1'){
+    //========Sorts choices in alphabetical order
+    console.log('ORDER!!!: ', order);
+		if(order === 'abcSort'){
 			choices = choices.split('\n').sort();
 		}
-
+    //============invokes $http request in app.module.js
 		projectFactory.createForm(label, default1, choices, order).then(function(result){
       console.log('createForm: ', result);
     });
 
 		console.log('label: ', label);
 		console.log('default: ', default1);
-		console.log('choice: ', choices);
+		console.log('choices sorted: ', choices);
 		console.log('order: ', order);
 	}
 
 //=========================Textarea and Div
   $scope.$watch('choice', function(newValue){
+    var tempString;
+    var newString = '';
+    newValue = newValue.split('\n');
+    //If newValue is undefined, it sets the div to an empty div
     if(newValue === undefined){
       $("#textareaDiv").html('<div></div>');
     }
-    var tempString;
-    var newString = '';
-    var newValue = newValue.split('\n');
 
+    
+    if(newValue.length > 5){
+      newValue.splice(4);
+      console.log('look here: ', newValue);
+    }
+
+    
     for(var i = 0; i < newValue.length; i++){
-      if(newValue[i].length === 0){
-        $scope.color = "black";
-      }
-
+      //if the string is greater than 4, it changes the characters colors to red
+      //that exceeds the threshold and then concats it back into newString
       if(newValue[i].length > 4){
+        //resultValue temporarily holds onto newValue's current index and splits it into an array
+        //resultValue is used, just so newValue is not modified, since it is needed to conduct a proper for loop
+        //it then splices at 2 and stores it into tempString
+        //result value is joined back into a string, minus the portion spliced off
 
         var resultValue = newValue[i].split('');
         tempString = resultValue.splice(2);
         resultValue = resultValue.join('');
         tempString = tempString.join('');
 
-        tempString = '<span style ="color: red">' + tempString + '</span>';
+        //temp string is referencing the portion spliced off, and it is joined back together
+        //it is now modified by being wraped in a span tag and then concated into a newString
+        tempString = '<span style ="color: red"><b>' + tempString + '</b></span>';
         newString = newString + ' '+ resultValue + tempString + '<br>';
       } else {
-
-        console.log('else: ', newString);
-        console.log('else: ', newValue[i]);
+        //if the current row is not longer than 4, it concats into the newString without being modified
         newString = newString + ' ' + newValue[i] + '<br>';
-        console.log('else after concat: ', newString);
       }
     }
-
+    //once the loop is finished executing, the newString is wrapped in a div and then rendered to the DOM
     $("#textareaDiv").html('<div>' + newString + '</div>');
   });
 }])
 //this directive limits the maxline in the users' choices box
+  //if the copies and pasts more than 5 lines, it invalidates the form
 .directive('maxlines', function() {
 	// http://stackoverflow.com/questions/26497492/limit-number-of-lines-or-rows-in-textarea
   return {
@@ -125,6 +126,25 @@ angular.module('project.homeView', ['ui.bootstrap', 'ngAnimate', 'ngSanitize','a
       }
 
       scope.$on('$destroy', removeKeypress);
+    }
+  };
+})
+//=====================This directive allows form to be validated if it is auto populated
+.directive('formAutofillFix', function() {
+  // http://victorblog.com/2014/01/12/fixing-autocomplete-autofill-on-angularjs-form-submit/
+  return function(scope, elem, attrs) {
+    // Fixes Chrome bug: https://groups.google.com/forum/#!topic/angular/6NlucSskQjY
+    elem.prop('method', 'POST');
+
+    // Fix autofill issues where Angular doesn't know about autofilled inputs
+    if(attrs.ngSubmit) {
+      setTimeout(function() {
+        elem.unbind('submit').submit(function(e) {
+          e.preventDefault();
+          elem.find('input, textarea, select').trigger('input').trigger('change').trigger('keydown');
+          scope.$apply(attrs.ngSubmit);
+        });
+      }, 0);
     }
   };
 });
